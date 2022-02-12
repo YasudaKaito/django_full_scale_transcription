f757c02dacc92c966b4d68f670a2c03c5cf0e57e
ykit <ykit.engineer@gmail.com>
2022-02-12 06:54:00 +0000

    p.32 add top html and test

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,18 +1,17 @@
-from django.http import HttpResponse
 from django.test import TestCase
 from django.urls import resolve
 
 from snippets.views import snippet_detail, snippet_edit, snippet_new
 
 
-class TopPageViewTest(TestCase):
-    def test_top_returns_200(self):
-        response: HttpResponse = self.client.get("/")
-        self.assertEqual(response.status_code, 200)
+class TopPageTest(TestCase):
+    def test_top_page_returns_200_and_title(self):
+        res = self.client.get("/")
+        self.assertContains(res, "Djangoスニペット", status_code=200)
 
-    def test_top_returns_expected_content(self):
-        response: HttpResponse = self.client.get("/")
-        self.assertEqual(response.content, b"Hello World")
+    def test_top_page_uses_expected_template(self):
+        res = self.client.get("/")
+        self.assertTemplateUsed(res, "snippets/top.html")
 
 
 class CreateSnippetTest(TestCase):
